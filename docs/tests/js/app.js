5a781dba98644c826b0996c6a6a458388f5b49e0
Jesse Vander <jessvand10@student.arteveldehs.be>
2020-05-23 17:14:08 +0200

    data-cases

--- docs/tests/js/app.js
+++ docs/tests/js/app.js
@@ -7,13 +7,6 @@
     console.log(jsonData);
   }
 
-  const getTarget = async (id) => {
-    const response = await fetch(`${DOMAIN}/data/blog/index.json`);
-    const jsonData = await response.json();
-    const target = jsonData.find(data => data.id === id);
-    console.log(target);
-  }
-
   const loadCases = async () => {
     const response = await fetch(`${DOMAIN}/data/cases/index.json`);
     const jsonData = await response.json();
@@ -22,5 +15,4 @@
 
   loadBlog();
   loadCases();
-  getTarget('10737437-307e-4232-b687-b03a0d7cfa18');
 })();
